7437700b9a002c0d089c70f6338a2bf71815893b
kandari_nitesh <niteshskandari@gmail.com>
2024-10-26 23:16:13 +0530

    changes in the cofig

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"https://job-posting-app-usinasd.netlify.app/",
   plugins: [react()],
   define: {
     'process.env': {}
